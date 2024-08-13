25adac5a884ee027a722f52c070e9456bc656913
Stephen Combs <scombs@paycor.com>
2024-08-13 15:35:19 -0400

    Configuration updates

--- dot_config/nvim/lua/config/executable_lazy.lua
+++ dot_config/nvim/lua/config/executable_lazy.lua
@@ -18,6 +18,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.angular" },
+    { import = "lazyvim.plugins.extras.lang.astro" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
 
     -- testing
     { import = "lazyvim.plugins.extras.test.core" },
--- dot_config/nvim/lua/plugins/executable_editor.lua
+++ dot_config/nvim/lua/plugins/executable_editor.lua
@@ -1,31 +1,31 @@
 local Util = require("lazyvim.util")
 
-local function biome_lsp_or_prettier(bufnr)
-  local has_biome_lsp = vim.lsp.get_active_clients({
-    bufnr = bufnr,
-    name = "biome",
-  })[1]
-  if has_biome_lsp then
-    return {}
-  end
-  local has_prettier = vim.fs.find({
-    -- https://prettier.io/docs/en/configuration.html
-    ".prettierrc",
-    ".prettierrc.json",
-    ".prettierrc.yml",
-    ".prettierrc.yaml",
-    ".prettierrc.json5",
-    ".prettierrc.js",
-    ".prettierrc.cjs",
-    ".prettierrc.toml",
-    "prettier.config.js",
-    "prettier.config.cjs",
-  }, { upward = true })[1]
-  if has_prettier then
-    return { "prettier" }
-  end
-  return { "biome" }
-end
+-- local function biome_lsp_or_prettier(bufnr)
+--   local has_biome_lsp = vim.lsp.get_active_clients({
+--     bufnr = bufnr,
+--     name = "biome",
+--   })[1]
+--   if has_biome_lsp then
+--     return {}
+--   end
+--   local has_prettier = vim.fs.find({
+--     -- https://prettier.io/docs/en/configuration.html
+--     ".prettierrc",
+--     ".prettierrc.json",
+--     ".prettierrc.yml",
+--     ".prettierrc.yaml",
+--     ".prettierrc.json5",
+--     ".prettierrc.js",
+--     ".prettierrc.cjs",
+--     ".prettierrc.toml",
+--     "prettier.config.js",
+--     "prettier.config.cjs",
+--   }, { upward = true })[1]
+--   if has_prettier then
+--     return { "prettier" }
+--   end
+--   return { "biome" }
+-- end
 
 return {
   {
@@ -164,6 +164,20 @@ return {
             },
           },
         },
+        vtsls = {
+          init_options = {
+            preferences = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+              importModuleSpecifierPreference = "non-relative",
+            },
+          },
+        },
       },
     },
   },
@@ -175,32 +189,32 @@ return {
       },
     },
   },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        ["javascript"] = biome_lsp_or_prettier,
-        ["javascriptreact"] = biome_lsp_or_prettier,
-        ["typescript"] = biome_lsp_or_prettier,
-        ["typescriptreact"] = biome_lsp_or_prettier,
-        ["vue"] = biome_lsp_or_prettier,
-        ["css"] = biome_lsp_or_prettier,
-        ["scss"] = biome_lsp_or_prettier,
-        ["less"] = biome_lsp_or_prettier,
-        ["html"] = biome_lsp_or_prettier,
-        ["json"] = biome_lsp_or_prettier,
-        ["jsonc"] = biome_lsp_or_prettier,
-        ["yaml"] = biome_lsp_or_prettier,
-        ["markdown"] = biome_lsp_or_prettier,
-        ["markdown.mdx"] = biome_lsp_or_prettier,
-        ["graphql"] = biome_lsp_or_prettier,
-        ["handlebars"] = biome_lsp_or_prettier,
-      },
-    },
-  },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       ["javascript"] = biome_lsp_or_prettier,
+  --       ["javascriptreact"] = biome_lsp_or_prettier,
+  --       ["typescript"] = biome_lsp_or_prettier,
+  --       ["typescriptreact"] = biome_lsp_or_prettier,
+  --       ["vue"] = biome_lsp_or_prettier,
+  --       ["css"] = biome_lsp_or_prettier,
+  --       ["scss"] = biome_lsp_or_prettier,
+  --       ["less"] = biome_lsp_or_prettier,
+  --       ["html"] = biome_lsp_or_prettier,
+  --       ["json"] = biome_lsp_or_prettier,
+  --       ["jsonc"] = biome_lsp_or_prettier,
+  --       ["yaml"] = biome_lsp_or_prettier,
+  --       ["markdown"] = biome_lsp_or_prettier,
+  --       ["markdown.mdx"] = biome_lsp_or_prettier,
+  --       ["graphql"] = biome_lsp_or_prettier,
+  --       ["handlebars"] = biome_lsp_or_prettier,
+  --     },
+  --   },
+  -- },
   {
     "supermaven-inc/supermaven-nvim",
-    build = ":SupermavenUseFree", -- remove this line if you are using pro
+    -- build = ":SupermavenUseFree", -- remove this line if you are using pro
     config = function()
       require("supermaven-nvim").setup({})
     end,
